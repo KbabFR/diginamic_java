fdaacdab9525e0e563c86151856e4f7ec16c3a30
Kbab <thomas.warin.pro@gmail.com>
2022-06-07 17:29:18 +0200

    fin du TP2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,30 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		byte a = 0;
+		short b = 0;
+		int c = 0;
+		long d = 0;
+		float e = 0;
+		double f = 0;
+		char g = '0';
+		boolean h = false;
+		String i = "0";
+		
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+		String randomString= "Voici le résultat d’un calcul :\n 1+5=6";
+		
+		System.out.println(randomString);
+		
+	}
+
+}
