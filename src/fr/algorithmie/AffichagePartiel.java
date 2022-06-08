1e44ff5fbb0705562bc2ada2736df31573208617
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-06-08 02:17:57 +0200

    maj

--- src/fr/algorithmie/AffichagePartiel.java
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -2,6 +2,9 @@ package fr.algorithmie;
 
 public class AffichagePartiel {
 
+	/*
+	 * Main method of AffichagePartiel
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
